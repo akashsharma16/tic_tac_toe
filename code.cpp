3b80c8dad0a8b33df8ae33b61b397bef0bdb5639
akashsharma16 <33964023+akashsharma16@users.noreply.github.com>
2020-09-30 18:26:19 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -56,14 +56,11 @@ void showBoard(vector<vector<char>> board)
 { 
     printf("\n\n"); 
     cout<<"----------------------------------------------------------------"<<endl;
-    printf("\t\t\t  %c | %c  | %c  \n", board[0][0], 
-                             board[0][1], board[0][2]); 
+    printf("\t\t\t  %c | %c  | %c  \n", board[0][0], board[0][1], board[0][2]); 
     printf("\t\t\t--------------\n"); 
-    printf("\t\t\t  %c | %c  | %c  \n", board[1][0], 
-                             board[1][1], board[1][2]); 
+    printf("\t\t\t  %c | %c  | %c  \n", board[1][0], board[1][1], board[1][2]); 
     printf("\t\t\t--------------\n"); 
-    printf("\t\t\t  %c | %c  | %c  \n\n", board[2][0], 
-                             board[2][1], board[2][2]); 
+    printf("\t\t\t  %c | %c  | %c  \n\n", board[2][0], board[2][1], board[2][2]); 
    cout<<"----------------------------------------------------------------"<<endl;
     return; 
 } 
